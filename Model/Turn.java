9add35701a56d9d176ad14eded235e17f77eebc2
jpdenford <jpdenford@gmail.com>
2015-07-31 22:10:03 +1200

    Added basic turn class - more methods to come

---
+++ Model/Turn.java
@@ -0,0 +1,18 @@
+package Model;
+
+/**
+ * A basic class for handling a players move
+ * @author Johnny
+ *
+ */
+public class Turn {
+	
+	Player player;
+	Menu menu;
+	public Turn(Player player, Menu menu) {
+		super();
+		this.player = player;
+		this.menu = menu;
+	}
+	
+}
